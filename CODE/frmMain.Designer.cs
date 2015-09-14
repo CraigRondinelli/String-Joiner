62c66e03a8a1bbe344c978eaa4a0029d80613ee7
CraigRondinelli <rondinelli.1337@gmail.com>
2015-09-14 14:00:51 -0700

    Initial Upload

---
+++ CODE/frmMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CODE
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtIn1 = new System.Windows.Forms.TextBox();
+            this.txtIn2 = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnJoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoin.Location = new System.Drawing.Point(223, 115);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(193, 38);
+            this.btnJoin.TabIndex = 2;
+            this.btnJoin.Text = "Join Strings";
+            this.btnJoin.UseVisualStyleBackColor = false;
+            this.btnJoin.Click += new System.EventHandler(this.butnJoin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(333, 339);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(227, 46);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(54, 339);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(227, 46);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtIn1
+            // 
+            this.txtIn1.Location = new System.Drawing.Point(21, 74);
+            this.txtIn1.Name = "txtIn1";
+            this.txtIn1.Size = new System.Drawing.Size(236, 20);
+            this.txtIn1.TabIndex = 0;
+            // 
+            // txtIn2
+            // 
+            this.txtIn2.Location = new System.Drawing.Point(363, 74);
+            this.txtIn2.Name = "txtIn2";
+            this.txtIn2.Size = new System.Drawing.Size(236, 20);
+            this.txtIn2.TabIndex = 1;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(21, 171);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(578, 143);
+            this.txtOut.TabIndex = 6;
+            this.txtOut.TabStop = false;
+            this.txtOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 413);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtIn2);
+            this.Controls.Add(this.txtIn1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnJoin);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtIn1;
+        private System.Windows.Forms.TextBox txtIn2;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ CODE/frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// Craig Rondinelli
+// CIS 202 - CODE
+
+//User enters an item into each textbox
+//Joins strings together on button click
+//Outputs result in bottom textbox
+
+namespace CODE
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //Titles the window upon opening.
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "Craig Rondinelli"; 
+        }
+
+        //Combines user inputted text using variables and outputs into textbox.
+
+        private void butnJoin_Click(object sender, EventArgs e)
+        {
+            string str1, str2, strOut; //Declares variables
+            str1 = txtIn1.Text;
+            str2 = txtIn2.Text;       //Defines variables
+            strOut = str1 + str2;
+            txtOut.Text = str1 + " + " + str2 + " = " + strOut; // Outputs user entered text and outcome into textbox using variables
+        }
+
+        // Clears all text from boxes
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtIn1.Clear();
+            txtIn2.Clear(); 
+            txtOut.Clear();
+        }
+
+        //Exits program
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+     
+    }
+}
